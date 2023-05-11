7f5cc3e0d8f7249584246ab99493d72c5a433b18
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-05-11 23:25:22 +0100

    Splve challenge according to requirements

--- morse.rb
+++ morse.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 CODES = {
   A: '.-',
   B: '-...',
@@ -27,7 +25,7 @@ CODES = {
   X: '-..-',
   Y: '-.--',
   Z: '--..',
-  " ": ' '
+  ' ': ' '
 }.freeze
 
 def decode_char(input)
