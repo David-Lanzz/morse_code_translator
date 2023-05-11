acf18e46f7a9c21b9a66829ad9d1a4fd9eb214f1
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-05-11 23:17:38 +0100

    Splve challenge according to requirements

--- morse.rb
+++ morse.rb
@@ -1,51 +1,55 @@
-CODES = {
-    A: '.-',
-    B: '-...',
-    C: '-.-.',
-    D: '-..',
-    E: '.',
-    F: '..-.',
-    G: '--.',
-    H: '....',
-    I: '..',
-    J: '.---',
-    K: '-.-',
-    L: '.-..',
-    M: '--',
-    N: '-.',
-    O: '---',
-    P: '.--.',
-    Q: '--.-',
-    R: '.-.',
-    S: '...',
-    T: '-',
-    U: '..-',
-    V: '...-',
-    W: '.--',
-    X: '-..-',
-    Y: '-.--',
-    Z: '--..'
-  }.freeze
+# frozen_string_literal: true
 
-  def decode_char(input)
-    CODES.each{
-        |key,value|
-    if value == input
-      print key
-    end
-    }
+CODES = {
+  A: '.-',
+  B: '-...',
+  C: '-.-.',
+  D: '-..',
+  E: '.',
+  F: '..-.',
+  G: '--.',
+  H: '....',
+  I: '..',
+  J: '.---',
+  K: '-.-',
+  L: '.-..',
+  M: '--',
+  N: '-.',
+  O: '---',
+  P: '.--.',
+  Q: '--.-',
+  R: '.-.',
+  S: '...',
+  T: '-',
+  U: '..-',
+  V: '...-',
+  W: '.--',
+  X: '-..-',
+  Y: '-.--',
+  Z: '--..',
+  " ": ' '
+}.freeze
+
+def decode_char(input)
+  CODES.each do |key, value|
+    print key if value == input
   end
+end
+
 def decode_word(input)
-    mess = input.split(" ")
-    mess.push(" ")
-    mess.each{|item|
-  "#{decode_char(item)} "
-    }
+  mess = input.split(' ')
+  mess.push(' ')
+  mess.each do |item|
+    decode_char(item)
+  end
 end
+
 def decode(input)
-    morse = input.split("   ") 
-    morse.each{|element|
-  "#{decode_word(element)}"
-    }
+  morse = input.split('   ')
+  morse.each  do |element|
+    decode_word(element)
+  end
 end
-decode("-- -.--   -. .- -- .")
+decode_char('--')
+decode_word('-- -.--')
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
